41fb4db86aae31cedf510691e7a80dd5283467b6
Oğuzhan Dereci <odereci@innova.com.tr>
2022-02-28 16:53:46 +0300

    Add project files.

---
+++ HepsiBurada.Case/Models/Rover.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HepsiBurada.Case.Models
+{
+    public class Rover
+    {
+        private int _coordX;
+        private int _coordY;
+        private char _heading;
+        private List<char[]> _commandsList;
+
+        public Rover(int coordX, int coordY, char heading)
+        {
+            _coordX = coordX;
+            _coordY = coordY;
+            _heading = heading;
+        }
+
+        public int CoordX { get; set; }
+        public int CoordY { get; set; }
+        public char Heading { get; set; }
+        public int CoordX { get; set; }
+
+
+    }
+}
